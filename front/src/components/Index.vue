cdca2d186be08daf68877baa1e19f3cc0648f332
Kaito Yamada <kite171@me.com>
2019-09-08 20:58:22 +0900

    グラフを少し追加した

--- front/src/components/Index.vue
+++ front/src/components/Index.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="index">
     <h1>{{ msg }}</h1>
+    <div class = "line">
+      <p>Bar Chart</p>
+      <chart :width="100" :height="50"></chart>
+    </div>
+    <div class = "radar">
+      <p>Radar Chart</p>
+      <radar :width="100" :height="50"></radar>
+    </div>
   </div>
 </template>
 
 <script>
+import Chart from './Chart';
+import Radar from './Radar';
+
 export default {
   name: 'Index',
+  props: ["options"],
+  components:{
+    Chart,
+    Radar
+  },
   data() {
     return{
       msg:"Visualizing-Baseball"
@@ -30,5 +46,17 @@ li {
 }
 a {
   color: #42b983;
+}
+.line{
+  margin-left: 2%;
+  float: left;
+  width: 45%;
+  height: 50%;
+}
+.radar{
+  margin-right: 2%;
+  float: right;
+  width: 45%;
+  height: 50%;
 }
 </style>
