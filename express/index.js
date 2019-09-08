24a0c34485fad64d73579588699c464b019638ff
Kaito Yamada <kite171@me.com>
2019-09-08 17:15:58 +0900

    expressの基本部分追加

---
+++ express/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+// const router = express.Router();
+const app = express() 
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgresql://localhost:5432/vb-db')
+
+app.get('/',function(req,res){
+    res.send('hello')
+})
+
+app.get('/hometeam', function (req, res) {
+    db.any("SELECT home FROM ballinfo;")
+      .then(function (data) {
+        res.json(data);
+    });
+});
+  
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log('server start listen on port ' + port);
+// module.exports = router;
