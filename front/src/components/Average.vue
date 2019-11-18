6dfa40c8424888cf2d76bd285e8890966d5c7080
Kaito Yamada <kite171@me.com>
2019-11-19 08:33:04 +0900

    AVG,HR,OPS追加

---
+++ front/src/components/Average.vue
@@ -0,0 +1,170 @@
+<template>
+  <v-container class="contain">
+    <v-card class="card" outlined>
+      <v-card-title class="title">
+        <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="200" offset-x>
+          <template v-slot:activator="{ on }">
+            <v-app-bar-nav-icon v-on="on" />
+          </template>
+          <v-card>
+            <v-card-text>チーム選択とかつける</v-card-text>
+          </v-card>
+        </v-menu>打率変化
+      </v-card-title>
+      <apexchart type="line" height="250" width="380" :options="chartOptions" :series="series" />
+      <v-row align="center" justify="space-around">
+        <v-col class="selector" cols="10">
+          <v-select
+            v-model="players"
+            v-on:change="updateChart(players)"
+            :items="players_data"
+            item-text="batter_jp"
+            item-value="batter"
+            chips
+            multiple
+            :append-icon="'mdi-plus'"
+            label="選手を選択"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="space-around">
+        <v-col cols="10">
+          <v-range-slider
+            v-on:change="updateChart(players)"
+            label="表示範囲"
+            step="0.05"
+            min="0"
+            max="1"
+            v-model="range"
+            thumb-label
+          ></v-range-slider>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+import Vue from "vue";
+import VueApexCharts from "vue-apexcharts";
+
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
+export default {
+  data: function() {
+    return {
+      menu: false,
+      players: [],
+      players_data: [],
+      range: [0, 0.5],
+      chartOptions: {
+        annotations: {
+          xaxis: [
+            {
+              x: new Date("4 Jun 2019").getTime(),
+              x2: new Date("25 Jun 2019").getTime(),
+              fillColor: "#B3F7CA",
+              opacity: 0.4,
+              label: {
+                borderColor: "#B3F7CA",
+                style: {
+                  fontSize: "10px",
+                  color: "#fff",
+                  background: "#00E396"
+                },
+                offsetY: -10,
+                text: "交流戦"
+              }
+            }
+          ]
+        },
+        xaxis: { categories: [], type: "datetime" },
+        yaxis: {
+          title: {
+            text: "Average"
+          },
+          min: 0,
+          max: 0.5
+        }
+      },
+      series: []
+    };
+  },
+  methods: {
+    updateChart(players) {
+      const chartOptions_data = {
+        annotations: {
+          xaxis: [
+            {
+              x: new Date("4 Jun 2019").getTime(),
+              x2: new Date("25 Jun 2019").getTime(),
+              fillColor: "#B3F7CA",
+              opacity: 0.4,
+              label: {
+                borderColor: "#B3F7CA",
+                style: {
+                  fontSize: "10px",
+                  color: "#fff",
+                  background: "#00E396"
+                },
+                offsetY: -10,
+                text: "交流戦"
+              }
+            }
+          ]
+        },
+        xaxis: { categories: [], type: "datetime" },
+        yaxis: {
+          title: {
+            text: "Average"
+          },
+          min: this.range[0],
+          max: this.range[1]
+        }
+      };
+      const series_data = [];
+
+      axios.get("https://vb-sql.herokuapp.com/B_AVG").then(response => {
+        const dates = response.data.map(x => Date.parse(x.GameDate));
+        chartOptions_data.xaxis.categories = dates;
+        players.forEach((p, i) => {
+          const bname = this.players_data.filter(el=>{
+              return el.batter === p
+          })
+          series_data.push({ name: bname[0].batter_jp, data: []});
+          response.data.forEach(el => {
+            series_data[i].data.push(el[`${p}`]);
+          });
+        });
+        this.chartOptions = chartOptions_data;
+        this.series = series_data;
+      });
+    }
+  },
+  created: function() {
+    axios.get("https://vb-sql.herokuapp.com/info/batter").then(response => {
+      this.players_data = response.data;
+    });
+  }
+};
+</script>
+
+<style scoped>
+.contain {
+  padding: 0px;
+  max-width: 400px;
+}
+.card {
+  margin: 0px;
+}
+.title {
+  padding-bottom: 0px;
+}
+.selector {
+  padding: 0px;
+}
+.v-text-field {
+  padding: 0px;
+}
+</style>
--- front/src/components/Index.vue
+++ front/src/components/Index.vue
@@ -1,138 +1,44 @@
 <template>
-  <v-container>
-    <div>
-      <v-card style="margin-top:100px;" class="mx-auto" max-width="600" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="headline mb-1">打率変化</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <apexchart type="line" height="300" :options="chartOptions" :series="series" />
-        <v-row align="center" justify="space-around">
-          <v-col cols="10">
-            <v-select
-              v-model="players"
-              v-on:change="updateChart(players)"
-              :items="items"
-              chips
-              multiple
-              :append-icon="'mdi-plus'"
-              label="選手を選択"
-            ></v-select>
-          </v-col>
-        </v-row>
-        <v-row align="center" justify="space-around">
-          <v-col cols="10">
-            <v-range-slider
-              v-on:change="updateChart(players)"
-              label="表示範囲"
-              step="0.05"
-              min="0"
-              max="1"
-              v-model="range"
-              thumb-label
-            ></v-range-slider>
-          </v-col>
-        </v-row>
-      </v-card>
-      <v-container style="margin-bottom: 100%"></v-container>
-    </div>
-  </v-container>
+  <!-- <v-container> -->
+  <div>
+    <v-row justify="center">
+      <v-col class="col">
+        <average></average>
+      </v-col>
+      <v-col class="col">
+        <ops></ops>
+      </v-col>
+      <v-col class="col">
+        <homerun></homerun>
+      </v-col>
+    </v-row>
+  </div>
+  <!-- </v-container> -->
 </template>
 
 <script>
-import axios from "axios";
-import Vue from "vue";
-import VueApexCharts from "vue-apexcharts";
+import average from "./Average.vue";
+import homerun from "./HR.vue";
+import ops from "./OPS.vue"
 
-Vue.use(VueApexCharts);
-Vue.component("apexchart", VueApexCharts);
 export default {
+  components: {
+    average,
+    homerun,
+    ops
+  },
   data: function() {
-    return {
-      items: [],
-      players: [],
-      range: [0, 1],
-      chartOptions: {
-        xaxis: {
-          categories: [],
-          type: "datetime"
-        },
-        yaxis: {
-          title: {
-            text: "Average"
-          },
-          min: 0,
-          max: 1
-        }
-      },
-      series: [
-        {
-          name: "series-1",
-          data: []
-        }
-      ]
-    };
+    return {};
   },
-  methods: {
-    updateChart(players) {
-      axios.get(`https://vb-sql.herokuapp.com/average/`).then(response => {
-        const chartOptions_data = {
-          annotations: {
-            xaxis: [
-              {
-                x: new Date("4 Jun 2019").getTime(),
-                x2: new Date("25 Jun 2019").getTime(),
-                fillColor: "#B3F7CA",
-                opacity: 0.4,
-                label: {
-                  borderColor: "#B3F7CA",
-                  style: {
-                    fontSize: "10px",
-                    color: "#fff",
-                    background: "#00E396"
-                  },
-                  offsetY: -10,
-                  text: "交流戦"
-                }
-              }
-            ]
-          },
-          xaxis: { categories: [], type: "datetime" },
-          yaxis: {
-            title: {
-              text: "Average"
-            },
-            min: this.range[0],
-            max: this.range[1]
-          }
-        };
-        const series_data = [];
-        players.forEach((player, i) => {
-          series_data.push({ name: player, data: [] });
-          response.data.forEach(element => {
-            chartOptions_data.xaxis.categories.push(
-              Date.parse(element.GameDate)
-            );
-            series_data[i].data.push(Math.round(element[player] * 1000) / 1000);
-          });
-        });
-        this.chartOptions = chartOptions_data;
-        this.series = series_data;
-      });
-    }
-  },
-  created: function() {
-    const index = [];
-    axios.get("https://vb-sql.herokuapp.com/average/").then(response => {
-      Object.keys(response.data[0]).forEach((el, i) => {
-        if (i >= 2) {
-          index.push(`${el}`);
-        }
-      });
-    });
-
-    this.items = index;
-  }
+  methods: {},
+  created: function() {}
 };
 </script>
+<style scoped>
+.col{
+  /* padding: 0px; */
+  max-width: 400px;
+  margin-top: 80px;
+  /* margin: 10px; */
+}
+</style>
